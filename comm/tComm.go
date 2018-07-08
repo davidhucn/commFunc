5f8e054e4a83aacb733da6b665054d1ba2ccff06
davidhucn <huhexu@hotmail.com>
2018-07-08 17:13:51 +0800

    first version:7/8/2018

---
+++ comm/tComm.go
@@ -0,0 +1,223 @@
+package comm
+
+import (
+	"bytes"
+	"eInfusion/logs"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//获取时间戳
+func GetTimeStamp() string {
+	return string(time.Now().Unix())
+}
+
+func GetRealIPAddr(ip string) string {
+	return strings.Split(ip, ":")[0]
+}
+
+// 获取当前时间
+func GetCurrentTime() string {
+	strTime := time.Now().Format("2006-01-02 15:04:05") //后面的参数是固定的 否则将无法正常输出
+	return strTime
+}
+
+//获取当前日期
+func GetCurrentDate() string {
+	return time.Now().Format("2006-01-02")
+}
+
+//生成分隔行
+func SepLi(ref_num int, ref_Char string) {
+	s := "-"
+	if ref_Char != "" {
+		s = ref_Char
+	}
+	Msg(strings.Repeat(s, ref_num))
+}
+
+//去除左右空格
+func TrimSpc(ref_str string) string {
+	return strings.TrimSpace(ref_str)
+}
+
+// 打印到屏幕
+func Msg(v ...interface{}) {
+	fmt.Println(v...)
+}
+
+//获取变量类型
+func GetVarType(ref_var interface{}) string {
+	return fmt.Sprint(reflect.TypeOf(ref_var))
+}
+
+//处理错误
+//如果有错误，返回true,无错则返回false
+func CkErr(ref_MsgTitle string, ref_err error) bool {
+	if ref_err != nil {
+		logs.LogMain.Error(ref_MsgTitle, ref_err)
+		return true
+	}
+	return false
+}
+
+//把数字内容（int/uint）根据参数base转换成指定进制，返回
+func ConvertBasNumberToStr(ref_intBase int, ref_varContent interface{}) string {
+	//	reflect.TypeOf(ref_varContent)
+	var strBaseValue string
+	switch ref_intBase {
+	case 16:
+		strBaseValue = "X"
+	case 10:
+		strBaseValue = "d"
+	case 2:
+		strBaseValue = "b"
+	case 1:
+		strBaseValue = "v"
+	default:
+		strBaseValue = "x"
+	}
+	strRetValue := fmt.Sprintf("%"+strBaseValue, ref_varContent)
+	return strRetValue
+}
+
+//转换16进制Bytes为string
+//转换过程中可能会丢失0，因此需要补0
+func ConvertOxBytesToStr(ref_content []byte) string {
+	var strRet string
+	for i := 0; i < len(ref_content); i++ {
+		strCon := ConvertBasNumberToStr(16, ref_content[i])
+		if len(strCon) == 1 {
+			strCon = "0" + strCon
+		}
+		strRet += strCon
+	}
+	return strRet
+}
+
+//	把指定进制的字符转换成为十进制数值（int型）
+func ConvertBasStrToInt(ref_intBase int, ref_content string) int {
+	intRetValue, err := strconv.ParseInt(ref_content, ref_intBase, 64)
+	if err != nil {
+		intRetValue = 0
+	}
+	return int(intRetValue)
+}
+
+//	根据指定进制要求，把字符串转换成数字Uint8型
+func ConvertBasStrToUint(ref_intBase int, ref_content string) uint8 {
+	intRetValue, err := strconv.ParseUint(ref_content, ref_intBase, 64)
+	if err != nil {
+		intRetValue = 0
+	}
+	return uint8(intRetValue)
+}
+
+//根据开始、结束下标返回相应的字符串内容返回bytes
+func ConvertBasStrToBytes(ref_strContent string, ref_intBegin int, ref_intEnd int, ref_intBase int) []byte {
+	var bT []byte
+	n := len(ref_strContent)
+	if ref_intEnd < n && ref_intBegin > 0 {
+		for i := ref_intBegin; i <= ref_intEnd; i++ {
+			strT := string(ref_strContent[i])
+			bT = append(bT, ConvertBasStrToUint(ref_intBase, strT))
+		}
+	}
+	return bT
+}
+
+//根据开始、结束下标返回相应的字符串内容返回string
+func GetPartOfStrToStr(ref_strContent string, ref_intBegin int, ref_intEnd int) string {
+	var strR string
+	n := len(ref_strContent)
+	if ref_intEnd < n && ref_intBegin >= 0 {
+		for i := ref_intBegin; i <= ref_intEnd; i++ {
+			strR += string(ref_strContent[i])
+		}
+	}
+	return strR
+}
+
+//把字符串内容按每两字符对应一个byte组成新的bytes，返回[]byte
+func ConvertPerTwoOxCharOfStrToBytes(ref_s string) []byte {
+	var bT []byte
+	var n int = len(ref_s)
+	for i := 0; i < n-1; i++ {
+		j := i + 1
+		strP := string(ref_s[i]) + string(ref_s[j])
+		bT = append(bT, ConvertBasStrToUint(16, string(strP)))
+	}
+	return bT
+}
+
+//判断是否存在,true表示存在
+func IsExists(ref_Path string) bool {
+	_, err := os.Stat(ref_Path)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// 获取当然路径
+func GetCurrentDirectory() string {
+	var strPath string
+	if os.IsPathSeparator('\\') { //前边的判断是否是系统的分隔符
+		strPath = "\\"
+	} else {
+		strPath = "/"
+	}
+	dir, _ := os.Getwd()
+	strPath = dir + strPath
+	return strPath
+}
+
+//根据环境，返回分隔符
+func GetPathSeparator() string {
+	var strPath string
+	if os.IsPathSeparator('\\') { //前边的判断是否是系统的分隔符
+		strPath = "\\"
+	} else {
+		strPath = "/"
+	}
+	return strPath
+}
+
+// 转化整形成字符型
+func ConvertIntToStr(intContent int) string {
+	return strconv.Itoa(intContent)
+}
+
+//整形转换成字节
+func ConvertIntToBytes(n int) []byte {
+	tmp := int32(n)
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, tmp)
+	return bytesBuffer.Bytes()
+}
+
+// 写入指定文件，如果没有该文件自动生成
+func WrToFilWithBuffer(f_strPath string, f_strContent string, f_boolWriteAppend bool) bool {
+	//	this function for complex content to file
+	var intFileOpenMode int
+	if f_boolWriteAppend {
+		intFileOpenMode = os.O_WRONLY | os.O_CREATE | os.O_APPEND
+	} else {
+		intFileOpenMode = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	}
+	fileHandle, err := os.OpenFile(f_strPath, intFileOpenMode, 0666)
+	if err != nil {
+		return false
+	} else {
+		// write to the file
+		fileHandle.WriteString("\r\n" + f_strContent)
+	}
+	defer fileHandle.Close()
+	return true
+}
---
+++ dbOperate/tMySql.go
@@ -0,0 +1,177 @@
+//***********************************************************
+//					 Package Name dbOperate				    *
+//					  File Name tMysql.go					*
+// 						Author:David.Hu						*
+//						Date:2018.03.22						*
+//				Remark:use the comm sql.Db object			*
+//***********************************************************
+package dbOperate
+
+import (
+	"database/sql"
+
+	_ "github.com/Go-SQL-Driver/MySQL"
+)
+
+const (
+	c_DB_IPAddr  = "127.0.0.1"
+	c_DB_Port    = "3306"
+	c_DB_schema  = "transfusion"
+	c_DB_UsrName = "root"
+	c_DB_Pwd     = "2341656"
+)
+
+var G_Db *sql.DB
+
+func init() {
+	var err error
+	strDataSource := c_DB_UsrName + ":" + c_DB_Pwd + "@tcp(" + c_DB_IPAddr + ":" + c_DB_Port + ")/"
+	strDataSource = strDataSource + c_DB_schema + "?charset=utf8"
+	G_Db, err = sql.Open("mysql", strDataSource)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func IsConnected() bool {
+	//	var dbStats sql.DBStats
+	if G_Db.Stats().OpenConnections > 0 {
+		return true
+	}
+	return false
+}
+
+//快速执行查询到指定数据库内
+func ExecSQL(strSql string, args ...interface{}) (affected_Num int64, err error) {
+	result, err := G_Db.Exec(strSql, args...)
+	if err != nil {
+		return 0, err
+	}
+	affected_Num, _ = result.RowsAffected()
+	return affected_Num, err
+}
+
+//插入数据到指定数据库内
+func InsertData(strSql string, args ...interface{}) (affected_Num int64, err error) {
+	var result sql.Result
+	stmtIns, err := G_Db.Prepare(strSql)
+	if err != nil {
+		return 0, err
+	}
+	defer stmtIns.Close()
+	result, err = stmtIns.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	affected_Num, _ = result.RowsAffected()
+	return affected_Num, err
+}
+
+//根据条件册除数据库
+func DeleteData(strSql string, args ...interface{}) (affected_Num int64, err error) {
+	var result sql.Result
+	stmtDel, err := G_Db.Prepare(strSql)
+	if err != nil {
+		return 0, err
+	}
+	defer stmtDel.Close()
+	result, err = stmtDel.Exec(args...)
+	affected_Num, _ = result.RowsAffected()
+	return affected_Num, err
+}
+
+//册除全表
+func TruncateTable(strTableName string) (affected_Num int64, err error) {
+	var result sql.Result
+	result, err = G_Db.Exec("Truncate Table " + strTableName)
+	if err != nil {
+		return 0, err
+	}
+	affected_Num, _ = result.RowsAffected()
+	return affected_Num, err
+}
+
+//查询单条数据,结果皆为string
+func QueryOneRow(strSql string, args ...interface{}) (*map[string]string, error) {
+
+	stmtOut, err := G_Db.Prepare(strSql)
+	if err != nil {
+		return nil, err
+	}
+	defer stmtOut.Close()
+	rows, err := stmtOut.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+	//获取字段对象
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	ret := make(map[string]string, len(scanArgs))
+
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return nil, err
+		}
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			ret[columns[i]] = value
+		}
+		break //get the first row only
+	}
+	return &ret, nil
+}
+
+//查询多条数据,结果皆为string
+func QueryRows(strSql string, args ...interface{}) (*[]map[string]string, error) {
+	stmtOut, err := G_Db.Prepare(strSql)
+	if err != nil {
+		return nil, err
+	}
+	defer stmtOut.Close()
+	rows, err := stmtOut.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+
+	ret := make([]map[string]string, 0)
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return nil, err
+		}
+		var value string
+		vmap := make(map[string]string, len(scanArgs))
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			vmap[columns[i]] = value
+		}
+		ret = append(ret, vmap)
+	}
+	return &ret, nil
+}
